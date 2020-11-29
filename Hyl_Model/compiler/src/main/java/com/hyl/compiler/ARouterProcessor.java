75fc2ac65090aa272ac8c0f524ceee5a2ba25684
haoyunlong <haoyunlong@xiaomi.com>
2020-11-29 22:01:53 +0800

    提交
    Signed-off-by: haoyunlong <haoyunlong@xiaomi.com>

--- Hyl_Model/compiler/src/main/java/com/hyl/compiler/ARouterProcessor.java
+++ Hyl_Model/compiler/src/main/java/com/hyl/compiler/ARouterProcessor.java
@@ -5,6 +5,9 @@ import com.google.auto.service.AutoService;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
@@ -12,14 +15,40 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import javax.tools.Diagnostic;
 
 @AutoService(Processor.class) //使用服务
 @SupportedAnnotationTypes("com.hyl.arouter_annotation.ARouter") //声明注解
-@SupportedSourceVersion(SourceVersion.RELEASE_7) //版本
+@SupportedSourceVersion(SourceVersion.RELEASE_8) //版本
 public class ARouterProcessor extends AbstractProcessor {
 
     // 操作Element的工具类（类，函数，属性，其实都是Element）
     private Elements elementTool;
+
+    // type(类信息)的工具类，包含用于操作TypeMirror的工具方法
+    private Types typeTool;
+
+    // Message用来打印 日志相关信息
+    private Messager messager;
+
+    // 文件生成器， 类 资源 等，就是最终要生成的文件 是需要Filer来完成的
+    private Filer filer;
+
+    private String options;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnvironment) {
+        super.init(processingEnvironment);
+        elementTool = processingEnvironment.getElementUtils();
+        messager = processingEnvironment.getMessager();
+        filer = processingEnvironment.getFiler();
+
+        String value = processingEnvironment.getOptions().get("student");
+
+        messager.printMessage(Diagnostic.Kind.NOTE, ">>>>>>>>>" + value);
+
+    }
 
     // 编译时注解
     @Override
