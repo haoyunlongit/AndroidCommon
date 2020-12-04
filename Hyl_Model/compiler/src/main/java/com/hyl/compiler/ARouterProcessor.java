69f79b50378418858883bb861ae955296933316d
haoyunlong <haoyunlong@xiaomi.com>
2020-12-04 09:25:26 +0800

    提交
    Signed-off-by: haoyunlong <haoyunlong@xiaomi.com>

--- Hyl_Model/compiler/src/main/java/com/hyl/compiler/ARouterProcessor.java
+++ Hyl_Model/compiler/src/main/java/com/hyl/compiler/ARouterProcessor.java
@@ -1,7 +1,11 @@
 package com.hyl.compiler;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 
+import java.io.IOException;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -13,6 +17,7 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
@@ -53,6 +58,30 @@ public class ARouterProcessor extends AbstractProcessor {
     // 编译时注解
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-        return false;
+
+        messager.printMessage(Diagnostic.Kind.NOTE, ">>>>runingruning>>>>>");
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                .build();
+
+        try {
+            javaFile.writeTo(System.out);
+        } catch (IOException e) {
+            e.printStackTrace();
+            messager.printMessage(Diagnostic.Kind.NOTE, "导入文件失败"+ e.getMessage());
+        }
+
+        return false; // false 不干活了  true 干完了
     }
 }
--- RxJava/app/src/main/java/com/hyl/rxjava/MainActivity.kt
+++ RxJava/app/src/main/java/com/hyl/rxjava/MainActivity.kt
@@ -2,17 +2,80 @@ package com.hyl.rxjava
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.View
+import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
-import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
 import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import org.reactivestreams.Subscriber
 
 class MainActivity : AppCompatActivity() {
+    var TAG = "MainActivity";
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var tempView = findViewById(R.id.my_text_view)
+        var tempView : View = findViewById(R.id.my_text_view)
+        tempView.setOnClickListener {
 
+
+        }
+
+    }
+
+    @SuppressLint("CheckResult")
+    fun demo1() {
+        Observable.create(ObservableOnSubscribe<String> {
+            it.onNext("fffffff")
+            it.onComplete()
+        }).subscribeOn(Schedulers.newThread()) // 置顶在子线程
+            .observeOn(AndroidSchedulers.mainThread()) //回调在主线程
+            .subscribe({
+                //结果
+            }, {
+                //异常
+            })
+    }
+
+    fun demo2() {
+        //创建一个观察者
+        //创建一个观察者
+        var observer: Observer<String?> = object : Observer<String?> {
+            override fun onSubscribe(d: Disposable) {
+                Log.i("hyl~~~", "onSubscribe =" + Looper.getMainLooper().isCurrentThread)
+            }
+
+            override fun onNext(t: String) {
+                Log.i("hyl~~~", "onNext + " + Looper.getMainLooper().isCurrentThread)
+            }
+
+            override fun onError(e: Throwable) {
+                Log.i("hyl~~~", "onError + " + Looper.getMainLooper().isCurrentThread)
+            }
+
+            override fun onComplete() {
+                Log.i("hyl~~~", "onComplete + " + Looper.getMainLooper().isCurrentThread)
+            }
+
+        }
+
+        //使用Observable.create()创建被观察者
+        //使用Observable.create()创建被观察者
+        val observable1: Observable<String> = Observable.create {
+            it.onNext("Hello" + Looper.getMainLooper().isCurrentThread)
+            it.onNext("Wrold" + Looper.getMainLooper().isCurrentThread)
+            Log.i("hyl~~~", "Observable + " + Looper.getMainLooper().isCurrentThread)
+            it.onComplete()
+        }
+        //订阅
+        //订阅
+        observable1.subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread()).subscribe(observer)
     }
 }
